c55d1cc8f8829d0f121ea52b25ecc0003308bc60
ROBOSHO <soarbear7@gmail.com>
2021-05-20 12:43:13 +0900

    Update create_rules.sh

--- scripts/create_rules.sh
+++ scripts/create_rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "remap the device serial port(ttyUSB*) to hayate_imu"
-echo "hayate_imu usb connection as /dev/hayate_imu, check it using command: ls -l /dev|grep ttyUSB"
+echo "remap the device serial port(ttyACM*) to hayate_imu"
+echo "hayate_imu usb connection as /dev/hayate_imu, check it using command: ls -l /dev|grep ttyACM"
 echo "start copy hayate_imu.rules to /etc/udev/rules.d/"
 echo "`rospack find hayate_imu_ros`/scripts/hayate_imu.rules"
 sudo cp `rospack find hayate_imu_ros`/scripts/hayate_imu.rules /etc/udev/rules.d
